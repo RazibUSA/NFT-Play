6910b7b5dd1dfa9466126bb634ece4a257d2bffc
Razib Mollick <rum.mollick@gmail.com>
2022-01-22 12:54:05 -0600

    Initial Commit

---
+++ nftPlay/nftPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  nftPlayApp.swift
+//  nftPlay
+//
+//  Created by Razib Mollick on 1/22/22.
+//
+
+import SwiftUI
+
+@main
+struct nftPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
